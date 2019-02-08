516b23f4860febd02561ef3d17b542e581bc74cc
“a_villarejo19” <a.villarejo19@gmail.com>
2019-02-08 17:35:37 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./polyfill";
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+
+// Styles
+// Import Flag Icons Set
+import "flag-icon-css/css/flag-icon.min.css";
+// Import Font Awesome Icons Set
+import "font-awesome/css/font-awesome.min.css";
+// Import Simple Line Icons Set
+import "simple-line-icons/css/simple-line-icons.css";
+// Import Main styles for this application
+import "../scss/style.scss";
+// Temp fix for reactstrap
+import "../scss/core/_dropdown-menu-right.scss";
+
+// Containers
+import Full from "./containers/Full/";
+
+
+
+export default class APP extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <Switch>
+          <Route path="/" name="Home" component={Full} />
+        </Switch>
+      </HashRouter>
+    );
+  }
+}
---
+++ src/components/Sidebar/_nav.js
@@ -0,0 +1,77 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW'
+      }
+    },
+    {
+      title: true,
+      name: 'Catalogos',
+      wrapper: {            // optional wrapper object
+        element: '',        // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: ''             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Usuarios',
+      url: '/usuarios',
+      icon: 'icon-people',
+    },
+    {
+      name: 'Giros',
+      url: '/grios',
+      icon: 'icon-people',
+    },
+    {
+      name: 'Formas Juridicas',
+      url: '/juridicas',
+      icon: 'icon-people',
+    },
+    {
+      name: 'Grados',
+      url: '/grados',
+      icon: 'icon-people',
+    },
+    {
+      name: 'Ambito Estatal',
+      url: '/ambitos',
+      icon: 'icon-drop',
+    },
+    {
+      name: 'Impuestos',
+      url: '/impuestos',
+      icon: 'icon-drop',
+    },
+    {
+      name: 'Contabilidad',
+      url: '/contabilidad',
+      icon: 'icon-drop',
+    },
+    {
+      name: `Tendencias Financieras`,
+      url: '/tendencias',
+      icon: 'icon-drop',
+    },
+    {
+      name: `Tipo de Comercialización`,
+      url: '/comercializacion',
+      icon: 'icon-drop',
+    },
+    {
+      name: 'Departamentos',
+      url: '/departamentos',
+      icon: 'icon-drop',
+    },
+    {
+      name: 'Empresas',
+      url: '/empresas',
+      icon: 'icon-drop',
+    }
+  ]
+};
